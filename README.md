49fd9dc57d29d5a99f5ba8a7bc77cc56600c0755
Mark Liederbach <10621536+markliederbach@users.noreply.github.com>
2020-08-04 13:49:55 -0500

    ✨ number guesser

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A simple repo to understand the basics of the Rust programming language.
 https://doc.rust-lang.org/book/ch01-03-hello-cargo.html
 
 - [Getting Started](#getting-started)
+  - [Run the application](#run-the-application)
 
 # Getting Started
 
+This project assumes you already have Rust installed.
+
+## Run the application
+```
+task cargo:run:debug
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101); // inclusive => exclusive
+
+    loop {
+        print!("Type your guess: ");
+        io::stdout().flush().unwrap(); // needed to flush the buffer
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // .parse() infers the type based on the annotation
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if guess.trim() == "quit" {
+                    println!("You quitter!");
+                    break;
+                };
+                eprintln!("Either enter a number or type 'quit' to exit");
+                continue;
+            }
+        };
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high, chief!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
